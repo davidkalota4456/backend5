7cf90639e2d86992342693723d050a322146f308
David Kalota <davidcalota54@gmail.com>
2024-07-27 13:09:38 +0300

    Initial commit

--- models/index.js
+++ models/index.js
@@ -1,22 +1,70 @@
-'use strict';
+//'use strict';
+//
+//
+//require('dotenv').config();
+//const fs = require('fs');
+//const path = require('path');
+//const Sequelize = require('sequelize');
+//const process = require('process');
+//const basename = path.basename(__filename);
+//const env = process.env.NODE_ENV || 'production';
+//const config = require(__dirname + '/../config/config.json')[env];
+//const db = {};
+//
+//let sequelize;
+//if (config.use_env_variable) {
+//  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+//} else {
+//  sequelize = new Sequelize(config.database, config.username, config.password, config);
+//}
+//
+//fs
+//  .readdirSync(__dirname)
+//  .filter(file => {
+//    return (
+//      file.indexOf('.') !== 0 &&
+//      file !== basename &&
+//      file.slice(-3) === '.js' &&
+//      file.indexOf('.test.js') === -1
+//    );
+//  })
+//  .forEach(file => {
+//    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+//    db[model.name] = model;
+//  });
+//
+//Object.keys(db).forEach(modelName => {
+//  if (db[modelName].associate) {
+//    db[modelName].associate(db);
+//  }
+//});
+//
+//
+//
+//
+//db.sequelize = sequelize;
+//db.Sequelize = Sequelize;
+//
+//module.exports = db;
 
+'use strict';
 
 require('dotenv').config();
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
-const process = require('process');
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'production';
-const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: 'mysql', // or 'postgres', 'sqlite', 'mariadb', etc.
+  }
+);
 
 fs
   .readdirSync(__dirname)
@@ -39,11 +87,7 @@ Object.keys(db).forEach(modelName => {
   }
 });
 
-
-
-
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
 module.exports = db;
-
